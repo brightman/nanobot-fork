6dc4598594322ed4ec492dc2f257539e594f6d4a
冯勇 <yong.feng@BJSK-A02706.local>
2026-02-24 17:02:01 +0800

    move self-upgrade out of core to external supervisor skill bridge

--- nanobot/supervisor/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Supervisor components for safe online upgrades."""
-
-from nanobot.supervisor.service import UpgradeRequest, UpgradeSupervisor, UpgradeTaskStore
-
-__all__ = ["UpgradeRequest", "UpgradeSupervisor", "UpgradeTaskStore"]
